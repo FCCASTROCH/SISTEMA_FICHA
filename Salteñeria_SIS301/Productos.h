62e9212241bcd85b89c68816d37343cf9aeee5d7
FIDEL CRISTIAN <castro.choque.fidelcristian@usfx.bo>
2025-05-05 23:52:20 -0400

    insertar imagen

--- Salteñeria_SIS301/Productos.h
+++ Salteñeria_SIS301/Productos.h
@@ -8,6 +8,8 @@ namespace SalteñeriaSIS301 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::IO;
+	using namespace System::Drawing::Imaging;
 
 	/// <summary>
 	/// Resumen de Productos
@@ -15,11 +17,13 @@ namespace SalteñeriaSIS301 {
 	public ref class Productos : public System::Windows::Forms::Form
 	{
 		Conexion^ can;
+		String^ selectedImagePath;
 	public:
 		Productos(void)
 		{
 			
 			can = Conexion::ObtenerConexion();
+			
 			InitializeComponent();
 			//
 			//TODO: agregar código de constructor aquí
@@ -46,6 +50,8 @@ namespace SalteñeriaSIS301 {
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
 
 	protected:
 
@@ -73,11 +79,14 @@ namespace SalteñeriaSIS301 {
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(116, 389);
+			this->button1->Location = System::Drawing::Point(75, 327);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(132, 23);
 			this->button1->TabIndex = 13;
@@ -87,7 +96,7 @@ namespace SalteñeriaSIS301 {
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(116, 201);
+			this->textBox3->Location = System::Drawing::Point(75, 139);
 			this->textBox3->Multiline = true;
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(373, 171);
@@ -95,14 +104,14 @@ namespace SalteñeriaSIS301 {
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(116, 150);
+			this->textBox2->Location = System::Drawing::Point(525, 392);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(373, 20);
+			this->textBox2->Size = System::Drawing::Size(211, 20);
 			this->textBox2->TabIndex = 11;
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(116, 93);
+			this->textBox1->Location = System::Drawing::Point(75, 82);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(373, 20);
 			this->textBox1->TabIndex = 10;
@@ -111,7 +120,7 @@ namespace SalteñeriaSIS301 {
 			// 
 			this->label3->AutoSize = true;
 			this->label3->BackColor = System::Drawing::Color::Transparent;
-			this->label3->Location = System::Drawing::Point(113, 185);
+			this->label3->Location = System::Drawing::Point(72, 123);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(169, 13);
 			this->label3->TabIndex = 9;
@@ -120,7 +129,7 @@ namespace SalteñeriaSIS301 {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(113, 134);
+			this->label2->Location = System::Drawing::Point(522, 376);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(49, 13);
 			this->label2->TabIndex = 8;
@@ -129,7 +138,7 @@ namespace SalteñeriaSIS301 {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(113, 77);
+			this->label1->Location = System::Drawing::Point(72, 66);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(135, 13);
 			this->label1->TabIndex = 7;
@@ -138,11 +147,29 @@ namespace SalteñeriaSIS301 {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(126, 40);
+			this->label4->Location = System::Drawing::Point(26, 42);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(93, 13);
 			this->label4->TabIndex = 14;
 			this->label4->Text = L"Publicar Campaña";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(614, 230);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 15;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Productos::button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(572, 42);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(164, 164);
+			this->pictureBox1->TabIndex = 16;
+			this->pictureBox1->TabStop = false;
 			// 
 			// Productos
 			// 
@@ -153,6 +180,8 @@ namespace SalteñeriaSIS301 {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
 			this->ClientSize = System::Drawing::Size(773, 495);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox3);
@@ -165,6 +194,7 @@ namespace SalteñeriaSIS301 {
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"Productos";
 			this->Text = L"Productos";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -184,13 +214,21 @@ namespace SalteñeriaSIS301 {
 		else if (descripcion == "") {
 
 			MessageBox::Show("Agrega una descripcion a la publicacion");
-		}
-		else if (imagen == "") {
+		}else {
+			if (selectedImagePath == nullptr || selectedImagePath->Length == 0) {
+				MessageBox::Show("Primero seleccione una imagen.");
+				return;
+			}
 
-			MessageBox::Show("Agrega el nombre de la imagen a la publicacion");
-		}
-		else {
-			can->insertarpublicacion(titulo, descripcion, imagen);
+			String^ fileName = Path::GetFileName(selectedImagePath);
+			String^ destinationPath = Path::Combine("C:/xampp/htdocs/fichashospita/public/images", fileName);
+			File::Copy(selectedImagePath, destinationPath, true); // Copia la imagen al directorio deseado
+
+		
+		
+
+			MessageBox::Show("Imagen guardada correctamente.");
+			can->insertarpublicacion(titulo, descripcion, fileName);
 			textBox1->Text = "";
 			textBox2->Text = "";
 			textBox3->Text = "";
@@ -199,5 +237,23 @@ namespace SalteñeriaSIS301 {
 		}
 
 	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	OpenFileDialog^ openFileDialog = gcnew OpenFileDialog();
+	openFileDialog->Filter = "Image Files|*.jpg;*.jpeg;*.png";
+	openFileDialog->Title = "Select an Image File";
+	if (openFileDialog->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+	{
+
+		selectedImagePath = openFileDialog->FileName;
+		pictureBox1->Image = Image::FromFile(selectedImagePath);
+		pictureBox1->SizeMode = PictureBoxSizeMode::StretchImage;
+
+
+	}
+	else
+	{
+		MessageBox::Show("No image selected.");
+	}
+}
 };
 }
--- Salteñeria_SIS301/inicio.cpp
+++ Salteñeria_SIS301/inicio.cpp
@@ -1,11 +1,23 @@
 #include "inicio.h"
-
 using namespace System;
 using namespace System::Windows::Forms;
-using namespace SalteñeriaSIS301;
-int main() {
 
-	Application::EnableVisualStyles();
-	Application::Run(gcnew inicio());
-	return 0;
+[STAThreadAttribute]
+int main(array<System::String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+   SalteñeriaSIS301::inicio form;
+    Application::Run(% form);
+    return 0;
 }
+//using namespace System;
+//using namespace System::Windows::Forms;
+//using namespace SalteñeriaSIS301;
+//int main() {
+//
+//	Application::EnableVisualStyles();
+//	Application::Run(gcnew inicio());
+//	return 0;
+//}
